3fa468026a6e1ccb5c12b4474537cad978330f6b
Hanivan <hanivan20@gmail.com>
2021-12-20 17:20:23 +0700

    fix installer #12

--- setup-scripts/aur-packages.sh
+++ setup-scripts/aur-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo -e "[START]: install extenal packages from aur..."
+exec_packages() {
+   helper=$1
+   download_source=$2
+   file_source=$3
+   echo -e "[START]: install extenal packages from aur..."
+   sudo pacman -Qi $helper || ./setup-scripts/.scripts/get-helper $download_source && $helper -S --noconfirm --useask --norebuild --needed --batchinstall --mflags --skipinteg --overwrite "*" --nodeps `cat $file_source` || exit 1
+}
+
 
 # Check if folder exist
 [ -d $HOME/Documents/git ] || mkdir $HOME/Documents/git
@@ -9,19 +16,19 @@ echo -e "[START]: install extenal packages from aur..."
 echo -e "Please select one for aur helpers:"
 echo "1. yay"
 echo "2. paru"
-echo "(1,2): "
-read -r helper
+read -p "(1,2): " helper
 
 while :
 do
+   shopt -s nocasematch
    case "$helper" in
       # Check if paru\yay is installed
-      "1" ) sudo pacman -Qi yay || ./setup-scripts/.scripts/get-helper https://aur.archlinux.org/yay.git && yay -S --noconfirm --useask --norebuild --needed --batchinstall --mflags --skipinteg --overwrite "*" --nodeps `cat ./setup-scripts/resources/aur-packages` || exit 1
-&& break ;;
-      "2" ) sudo pacman -Qi paru || ./setup-scripts/.scripts/get-helper https://aur.archlinux.org/paru.git && paru -S --noconfirm --useask --norebuild --needed --batchinstall --mflags --skipinteg --overwrite "*" --nodeps `cat ./setup-scripts/resources/aur-packages` || exit 1
-&& break ;;
-      *) echo -e "Option not found. Try again..."
+      [1] ) exec_packages "yay" "https://aur.archlinux.org/yay.git" "./setup-scripts/resources/aur-packages" && break ;;
+      [2] ) exec_packages "paru" "https://aur.archlinux.org/paru.git" "./setup-scripts/resources/aur-packages" && break ;;
+      *) echo -e "Option not found. Try again..." ;;
    esac
+   sleep 1
+   read -p "(1,2): " helper
 done
 
 echo -e "[FINISHED]: install external packages"
