22a261d8a869a4d05ed44a5de45262a76f37f6ee
Hanivan <hanivan20@gmail.com>
2021-12-20 20:37:29 +0700

    fix installer #2

--- setup-scripts/rices.sh
+++ setup-scripts/rices.sh
@@ -118,9 +118,9 @@ config_zsh() {
    sudo pacman -Qi zsh || yes | sudo pacman -Sy zsh
    # oh-my-zsh
    yes "n" | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+   git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-   git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
    chsh -s $(which zsh)
    echo $SHELL
