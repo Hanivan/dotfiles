383e555e204a5eed545ff47edc353676300bf546
Hanivan <hanivan20@gmail.com>
2021-12-20 14:21:55 +0700

    fix installer #3

--- setup-scripts/install-drivers.sh
+++ setup-scripts/install-drivers.sh
@@ -4,10 +4,10 @@ echo -e "Please select your graphic card type"
 echo "1. amdgpu"
 echo "2. intel"
 echo "3. nvidia"
-read -p "(1,2,3): "
+read -p "(1,2,3): " model
 
-# while :
-# do
+while :
+do
    shopt -s nocasematch
    case "$model" in
       [1] ) sudo pacman -Sy `cat ./setup-scripts/resources/drivers-amdgpu` && break ;;
@@ -15,5 +15,6 @@ read -p "(1,2,3): "
       [3] ) sudo pacman -Sy `cat ./setup-scripts/resources/drivers-nvidia` && break ;;
       *) echo -e "Options not found. try again.."
    esac
-   # pause 1
-# done
+   pause 1
+   read -p "(1,2,3): " model
+done
