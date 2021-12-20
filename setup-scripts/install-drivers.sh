09049b5b8a9afa0c3d6c42adfab12004d2d3ac45
Hanivan <hanivan20@gmail.com>
2021-12-20 17:09:31 +0700

    fix installer #11

--- setup-scripts/install-drivers.sh
+++ setup-scripts/install-drivers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+exec_packages() {
+   file_source=$1
+   yes | sudo pacman -Sy --needed --overwrite "*" --nodeps --nodeps `cat $file_source` || exit 1
+}
+
 echo -e "Please select your graphic card type"
 echo "1. amdgpu"
 echo "2. intel"
@@ -10,10 +15,10 @@ while :
 do
    shopt -s nocasematch
    case "$model" in
-      [1] ) sudo pacman -Sy `cat ./setup-scripts/resources/drivers-amdgpu` && break ;;
-      [2] ) sudo pacman -Sy `cat ./setup-scripts/resources/drivers-intel` && break ;;
-      [3] ) sudo pacman -Sy `cat ./setup-scripts/resources/drivers-nvidia` && break ;;
-      *) echo -e "Options not found. Try again.."
+      [1] ) exec_packages "./setup-scripts/resources/drivers-amdgpu" && break ;;
+      [2] ) exec_packages "./setup-scripts/resources/drivers-intel" && break ;;
+      [3] ) exec_packages "./setup-scripts/resources/drivers-nvidia" && break ;;
+      *) echo -e "Options not found. Try again.." ;;
    esac
    sleep 1
    read -p "(1,2,3): " model
