7bc0fb949075578060bad713f8aa3b0b19c0b534
Hanivan <hanivan20@gmail.com>
2021-12-20 16:59:23 +0700

    fix installer #7

--- setup-scripts/pacman-packages.sh
+++ setup-scripts/pacman-packages.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+exec_packages() {
+   file_source=$1
+   echo -e "[START]: install general packages..."
+   yes | sudo pacman -Syu
+   yes | sudo pacman -Sy --needed --overwrite "*" --nodeps --nodeps `cat $file_source` || exit 1
+}
+
 echo -e "Which one your system init?"
 echo "1. SystemD"
 echo "2. runit"
 read -p "(1,2): " init
 
-echo -e "[START]: install general packages..."
-
 while :
 do
    shopt -s nocasematch
    case "$init" in
-      [1] ) 
-         yes | sudo pacman -Syu
-         yes | sudo pacman -Sy --needed --overwrite "*" --nodeps --nodeps `cat ./setup-scripts/resources/pacman-packages` || exit 1 ;;
-      [2] )
-         yes | sudo pacman -Syu
-         yes | sudo pacman -Sy --needed --overwrite "*" --nodeps --nodeps `cat ./setup-scripts/resources/pacman-packages ./setup-scripts/resources/pacman-packages-runit` || exit 1 ;;
-      *) echo -e "Option not found. Try again..."
+      [1] ) exec_packages "./setup-scripts/resources/pacman-packages" ;;
+      [2] ) exec_packages "./setup-scripts/resources/{pacman-packages,pacman-packages-runit}" ;;
+      *) echo -e "Option not found. Try again..." ;;
    esac
    sleep 1
    read -p "(1,2): " init
